9ea0937d61ed610457fbad11e2b5a471a795ee88
Arjunb4 <ba5897613@gmail.com>
2026-01-18 18:46:41 +0530

    call

---
+++ app/volunteercall/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {redirect} from 'next/navigation'
+
+function page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSfg3HIrWWMduf5Ykge6PSD_FSsjNcTrhMT8n8rWQyL0TG-9wg/viewform?usp=sharing&ouid=107078352820636990092');
+}
+
+export default page
